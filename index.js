e75bbcf8c05b056e911e66aab5c94c248de0dd0e
hiuhuedd <hiuhuedd@gmail.com>
2023-07-08 19:09:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+import express from 'express';
+import https from 'https';
+import fs from 'fs';
+import { createWriteStream } from 'fs';
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABDRRPeRUvBvm6NgfuK1ctmLTleDxdyWg",
+    authDomain: "campus360-e88ab.firebaseapp.com",
+    projectId: "campus360-e88ab",
+    storageBucket: "campus360-e88ab.appspot.com",
+    messagingSenderId: "868564611977",
+    appId: "1:868564611977:web:b559bbe811cd2ca2972758",
+    measurementId: "G-KJ91P1JX3V"
+};
+
+const app = express();
+const port = 3000;
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
+
+app.get('/', (req, res) => {
+  console.log("We're here");
+  res.send("Hello from the server!");
+});
+
+app.post('/text-to-speech', (req, res) => {
+  const options = {
+    hostname: 'api.elevenlabs.io',
+    path: '/v1/text-to-speech/pNInz6obpgDQGcFmaJgB/stream',
+    method: 'POST',
+    headers: {
+      'accept': 'audio/mpeg',
+      'xi-api-key': '9c1a3cf1d0cba9691eb80d35596ca8f8',
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const request = https.request(options, (response) => {
+    console.log(`statusCode: ${response.statusCode}`);
+
+    const audioFile = createWriteStream('audio.mp3');
+   
+    audioFile.on('finish', () => {
+      const audRef = ref(storage, `audios/${Math.random()}.mp3`);
+      uploadBytes(audRef, fs.readFileSync('audio.mp3')).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          console.log(url);
+          res.send(url);
+        });
+      }).catch((error) => {
+        console.error('Error uploading audio:', error);
+        res.status(500).send('Error occurred');
+      });
+    });
+
+    response.on('end', () => {
+      console.log('new Audio file saved successfully');
+    });
+  });
+
+  request.on('error', (error) => {
+    console.error(error);
+    res.status(500).send('Error occurred');
+  });
+
+  const text = `Hi Edward! welcome to three sixty AI... I'm your personal assistant. you can ask me anything `;
+  const voiceSettings = {
+    stability: 0.5,
+    similarity_boost: 0.5
+  };
+
+  request.write(JSON.stringify({
+    text: text,
+    voice_settings: voiceSettings
+  }));
+
+  request.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
